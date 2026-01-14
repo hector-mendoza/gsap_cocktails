ac3c4c09171faeac6674e0d7a0a75c5d2fc0448c
Hector Mendoza <hey@hectormendoza.me>
2026-01-14 10:18:43 -0600

    adding navbar gsap effect + public folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
+import { useGSAP } from "@gsap/react"
 import { navLinks } from "../../constants"
+import gsap from "gsap"
 
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: "nav",
+                start: "bottom top",
+            }
+        });
+
+        navTween.fromTo('nav', {
+            backgroundColor: "transparent",
+        }, {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            backdropFilter: "blur(10px)",
+            duration: 1,
+            ease: "power1.out",
+        })
+    })
+
     return (
         <nav>
             <div>
                 <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" />
                     <p>Velvet Pour</p>
                 </a>
 
