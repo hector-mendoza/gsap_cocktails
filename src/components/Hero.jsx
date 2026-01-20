f968308463e59a6d4f8cf25ff38dbe982373ef34
Hector <hey@hectormendoza.me>
2026-01-20 11:07:06 -0600

    Implement Hero Animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         // GSAP animations will go here
         const heroSplit = new SplitText(".title", { type: "chars, words" });
@@ -40,6 +45,26 @@ const Hero = () => {
         })
             .to(".right-leaf", { y: 200 }, 0)
             .to(".left-leaf", { y: -200 }, 0)
+
+        // top of the video reaches 50% of vp height on mobile, center reaches 60% on desktop
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
     }, []);
 
     return (
@@ -68,6 +93,16 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     )
 }
